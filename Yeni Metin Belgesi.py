d15049b2f13a1a39452704a7e226699b9389c28e
HamzaKaraermis <hamzakaraermis6706@gmail.com>
2025-10-16 16:35:09 +0300

    hesap makinesi

--- Yeni Metin Belgesi.py
+++ Yeni Metin Belgesi.py
@@ -1,15 +1,24 @@
 '''
  Yazar:Hamza Karaermiş
  E-mail: hamzakaraermis@gmail.com 
- Açıklama:
+ Açıklama: try-except kullanımı
 
 '''
 if __name__== '__main__':
     try:
-        sayı1 = int(input("Birinci sayıyı giriniz: "))
-        sayı2 = int(input("İkinci sayıyı giriniz: "))
-        print (sayı1 ,''  ---- '', sayı2 )
-    except:
-        print("Lütfen sadece tam sayı giriniz.")
-        
-        
+     sayı1 = float(input("Birinci sayıyı giriniz: "))
+     sayı2 = float(input("İkinci sayıyı giriniz: "))
+    except ValueError:
+     print("Lütfen sadece sayı giriniz.")
+    else:
+     işlem = input("Yapmak istediğiniz işlemi giriniz(+,-,*,/): ")
+     if işlem == "+":
+        print(sayı1, "+", sayı2, "=", sayı1 + sayı2)
+     elif işlem == "-":
+        print(sayı1, "-", sayı2, "=", sayı1 - sayı2)
+     elif işlem == "*":
+        print(sayı1, "*", sayı2, "=", sayı1 * sayı2)
+     elif işlem == "/":
+       print(sayı1, "/", sayı2, "=", sayı1 / sayı2)
+     else:
+        print("Belirtilen işlemlerden birini giriniz.")
